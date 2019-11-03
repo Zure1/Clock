58500cbe052d8720d420797f51ede3b588ad5305
Dennis Schröer <Luchsia@Freenet.de>
2019-11-03 15:24:03 +0100

    First Commit

---
+++ Assets/Clock.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    public Transform hoursTransform, minutesTransform, secondsTransform;
+    float degreesPerHour = 30f, degreesPerMinute = 6f, degreesPerSecond = 6f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DateTime currentDateTime = DateTime.Now;
+        TimeSpan time = DateTime.Now.TimeOfDay;
+
+        hoursTransform.localRotation = Quaternion.Euler(0f, (float)time.TotalHours * degreesPerHour, 0f);
+        minutesTransform.localRotation = Quaternion.Euler(0f, (float)time.TotalMinutes * degreesPerMinute, 0f);
+        secondsTransform.localRotation = Quaternion.Euler(0f, (float)time.TotalSeconds * degreesPerSecond, 0f);
+    }
+}
